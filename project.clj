ce66f7081cb5c6e0c9d16ebdf07bb9e661df6baf
Dimitris <jimpil1985@gmail.com>
2017-06-27 20:24:32 +0100

    mark 0.1.0 version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ajenda "0.1.0-SNAPSHOT"
+(defproject ajenda "0.1.0"
   :description "A bunch of utilities for scheduling side effects."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jimpil/ajenda"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
