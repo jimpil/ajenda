3b7b77370d38a2d461b47a9a4e5d765f05d025fd
Dimitris <jimpil1985@gmail.com>
2017-10-24 17:49:30 +0100

    simplify implementation of `rate-limited` and add support for a `log-fn`

--- src/ajenda/limiting.clj
+++ src/ajenda/limiting.clj
@@ -24,42 +24,45 @@
    <n-calls> times during the specified <time-unit> (java.util.concurrent.TimeUnit).
    Any further calls will return nil without calling <f> (noop). As soon as the specified
    <time-unit> passes, new calls will start calling <f> again."
-  [f [n-calls ^TimeUnit time-unit n-units :as rate]]
-  (let [times-called (AtomicLong. 0)
-        nanos-per-unit (.toNanos time-unit (or n-units 1))
-        t (AtomicLong. 0)  ;; not been called yet
-        do-f (fn [first-time? args]
-               (try (apply f args)
-                    (finally
-                      (if first-time?
-                        (.addAndGet times-called 2) ;; go from 0 => 2 in order to simulate going from 1 => 2
-                        (.incrementAndGet times-called))))) ;; go from n => n+1
-        ]
 
-    (fn [& args]
-      (let [calls-so-far (.get times-called)
-            first-time? (zero? calls-so-far)
-            now (System/nanoTime)
-            previous-time (if first-time?
-                            ;; very first time round - initialise it properly
-                            (.addAndGet t now)
-                            (.get t))
-            time-elapsed (- now previous-time)]
-        ;(println time-elapsed)
+  ([f rate]
+   (rate-limited f rate (constantly nil)))
+  ([f [n-calls ^TimeUnit time-unit n-units :as rate] log-fn]
+   (let [times-called (AtomicLong. 0) ;; not been called yet
+         nanos-per-unit (.toNanos time-unit (or n-units 1))
+         t (AtomicLong. 0)
+         do-f (fn [args]
+                (try (apply f args)
+                     (finally
+                       (log-fn (.incrementAndGet times-called))))) ;; go from n => n+1
+         ]
 
-        (if (>= time-elapsed nanos-per-unit)
-          (do ;; reset everything
-            ;(println "resetting everything...")
-            (.set times-called 1) ;; don't start from 0 now
-            (.set t now)
-            ;; don't forget to call <f>
-            (do-f first-time? args))
+     (fn [& args]
+       (let [previous-time* (.get t)
+             calls-so-far (.get times-called)
+             first-time? (zero? previous-time*) ;; this will be true the very first time only
+             now (System/nanoTime)
+             previous-time (if first-time?
+                             ;; very first time being called - initialise it properly
+                             (.addAndGet t now)
+                             previous-time*)
+             time-elapsed (- now previous-time)]
+         ;(println time-elapsed)
 
-          (when (>= n-calls calls-so-far)
-            ;; all good -call <f>
-            (do-f first-time? args))
-          )
-        )
-      )
+         (if (>= time-elapsed nanos-per-unit)
+           (do ;; reset everything
+             ;(println "resetting everything...")
+             (.set times-called 0)
+             (.set t now)
+             ;; don't forget to call <f>
+             (do-f args))
+
+           (when (> n-calls calls-so-far) ;; call count starts at 0 so use `>`
+             ;; all good -call <f>
+             (do-f args))
+           )
+         )
+       )
+     )
     )
   )
--- test/ajenda/limiting_test.clj
+++ test/ajenda/limiting_test.clj
@@ -4,17 +4,22 @@
   (:import (java.util.concurrent.atomic AtomicInteger)
            (java.util.concurrent TimeUnit)))
 
+(defn- log-attempt! [i]
+  (println "Attempt:" i))
+
 
 (deftest rate-limited-tests
   (let [checkbox1 (AtomicInteger. 0)
         checkbox2 (AtomicInteger. 0)
         f1 (rate-limited (fn []
                            (.incrementAndGet checkbox1))
-                         [5 TimeUnit/MILLISECONDS 500])
+                         [5 TimeUnit/MILLISECONDS 500]
+                         log-attempt!)
         f2 (rate-limited (fn []
                            (.incrementAndGet checkbox2)
                            (Thread/sleep 1000)) ;; guaranteed to succeed
-                         [1 TimeUnit/SECONDS 1])
+                         [1 TimeUnit/SECONDS 1]
+                         log-attempt!)
         ]
     (testing ""
       (dotimes [_ 10]
@@ -26,7 +31,7 @@
 
     (dotimes [_ 5]
       (f2))
-    (is (= 5 (.get checkbox2))) ;; all calls had an effect
+    (is (= 5 (.get checkbox2))) ;; all calls had an effect because the call itself is sleeping for 1000ms
     )
 
 
